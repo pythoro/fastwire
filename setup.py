a8e00204809d4c208692ac5f6ccab09b23bc9345
pythoro <pythoro@mindquip.com>
2019-07-22 12:48:56 +1200

    [IMP] Improve project metadata.

--- setup.py
+++ setup.py
@@ -12,14 +12,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastwire",
-    version="0.1",
+    version="0.1.1",
     author="Reuben Rusk",
     author_email="pythoro@mindquip.com",
     description="Easy data transfer between classes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pythoro/fastwire.git",
+    download_url="https://github.com/pythoro/fastwire/archive/v0.1.1.zip",
     packages=['fastwire'],
+    keywords=['WIRE', 'CONNECTION', 'UTILITY'],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",

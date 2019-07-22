f3cedec5c3ad06891df65a718bf80a50122b684f
pythoro <pythoro@mindquip.com>
2019-07-22 20:16:21 +1200

    [MOD] Change version for release.

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastwire",
-    version="0.1.1",
+    version="0.1.2",
     author="Reuben Rusk",
     author_email="pythoro@mindquip.com",
     description="Easy data transfer between classes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pythoro/fastwire.git",
-    download_url="https://github.com/pythoro/fastwire/archive/v0.1.1.zip",
+    download_url="https://github.com/pythoro/fastwire/archive/v0.1.2.zip",
     packages=['fastwire'],
     keywords=['WIRE', 'CONNECTION', 'UTILITY'],
     classifiers=[

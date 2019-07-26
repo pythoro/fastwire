7e24504c523b7c81af854371d301292fafb93b7e
pythoro <pythoro@mindquip.com>
2019-07-27 09:43:06 +1200

    [DOC] Add docstrings for wire and signal classes.

--- fastwire/signal.py
+++ fastwire/signal.py
@@ -3,28 +3,43 @@
 Created on Sat Jul 27 08:56:13 2019
 
 @author: Reuben
+
+Signals can be connected to multiple callable receivers. The hold weak
+references to those callables, so that when they are garbage collected their
+references are removed automatically.
+
 """
+
 
 import weakref
 
 class SignalContainer(dict):
-    ''' Holds a collection of signals '''
+    ''' A dictionary-like collection of Signal instances '''
     
     def signal(self, name=None, **kwargs):
+        ''' Get a new Signal instance 
+        
+        Args:
+            name (str): A name of the wire [optional]
+            doc (str): A documentation string for the wire [optional]
+        '''
         name = len(self) if name is None else name
         s = Signal(name=name, **kwargs)
         self[name] = s
         return s
     
     def mute_all(self):
+        ''' Mute all signals in the container '''
         for key, signal in self.items():
             signal.mute()
 
     def unmute_all(self):
+        ''' Unmute all signals in the container '''
         for key, signal in self.items():
             signal.unmute()
 
     def reset_all(self):
+        ''' Reset all wires in the contain '''
         for key, signal in self.items():
             signal.reset()
     
@@ -34,7 +49,14 @@ signal = default_container.signal
 
     
 class Signal():
-    ''' The core signal class '''
+    ''' A class that can emit and receive data from multiple callables. 
+    
+        Args:
+            name (str): A name of the wire [optional]
+            doc (str): A documentation string for the wire [optional]
+            receiver_limit (int): Limit the number of receivers [optional]
+            condition (Condition): An optional signal Condition.
+    '''
     
     def __init__(self, name=None, doc=None, receiver_limit=None, condition=None):
         self.reset()
@@ -45,13 +67,21 @@ class Signal():
             self.add_condition(condition)
     
     def add_condition(self, condition):
-        ''' Add a condition that the signal must pass to be received '''
+        ''' Add a condition that the signal must pass to be received
+        
+        Args:
+            condition (Condition): A Condition instance
+        '''
         self._conditions[condition.name] = condition
         self.emit = self._conditioned_emit
         return True
         
     def remove_condition(self, name):
-        ''' Remove a condition that the signal must pass to be received '''
+        ''' Remove a condition that the signal must pass to be received 
+        
+        Args:
+            name (str): The name of the condition to remove.
+        '''
         try:
             del self._conditions[name]
         except KeyError:
@@ -60,7 +90,15 @@ class Signal():
             self.emit = self._emit
     
     def connect(self, receiver, **receiver_kwargs):
-        ''' Store weakref of receiver function or method to call '''
+        ''' Store weakref of receiver function or method to call 
+        
+        Args:
+            receiver (callable): A callable receiver
+            kwargs: Optional key word arguments
+            
+        Returns:
+            float: A receiver id that can be used to disconnect
+        '''
         if self.n == self._receiver_limit:
             raise KeyError('Limit of receivers (or suppliers) reached.')
         receiver_id = self._next_id
@@ -92,7 +130,11 @@ class Signal():
         return self._name
     
     def disconnect(self, receiver_id):
-        ''' Disconnect a receiver '''
+        ''' Disconnect a receiver 
+        
+        Args:
+            receiver_id (int): The id of the receiver.
+        '''
         try:
             del self._receivers[receiver_id]
             del self._receiver_kwargs[receiver_id]
@@ -102,6 +144,9 @@ class Signal():
 
     def _emit(self, **kwargs):
         ''' The standard emit method 
+
+        Args:
+            **kwargs: Key word arguments.
         
         Note: The 'emit' method is set to this method normally.
         '''
@@ -110,7 +155,11 @@ class Signal():
             receiver(**kwargs)
             
     def _conditioned_emit(self, **kwargs):
-        ''' A conditioned emit method '''
+        ''' A conditioned emit method 
+        
+        Args:
+            **kwargs: Key word arguments.
+        '''
         for receiver_id, ref in self._receivers.items():
             condition_pass = True
             for condition_name, condition in self._conditions.items():
@@ -124,15 +173,21 @@ class Signal():
         pass
             
     def mute(self):
+        ''' Prevent receivers from receiving signals '''
         self._prev_emit = self.emit
         self.emit = self._muted
 
     def unmute(self):
+        ''' Allow receivers to receive signals (after a mute) '''
         self.emit = self._prev_emit
         del self._prev_emit
     
     def fetch(self, **kwargs):
-        ''' Get a return value from a single supplier '''
+        ''' Get a return value from a single supplier 
+        
+        Args:
+            **kwargs: Key word arguments.
+        '''
         if self._receiver_limit != 1:
             raise KeyError('Signal must be set to have only 1 supplier.')
         if self.n == 0:
@@ -142,7 +197,14 @@ class Signal():
             return receiver(**kwargs)
 
     def fetch_all(self, **kwargs):
-        ''' Get a return value from a single supplier '''
+        ''' Get return value from all connected callables 
+        
+        Args:
+            **kwargs: Key word arguments.        
+        
+        Returns:
+            list: The list of return values
+        '''
         if self.n == 0:
             raise KeyError('No suppliers')
         ret = []
@@ -152,6 +214,7 @@ class Signal():
         return ret
 
     def reset(self):
+        ''' Reset the signal '''
         self._receivers = {}
         self._receiver_kwargs = {}
         self._next_id = 0
--- fastwire/wire.py
+++ fastwire/wire.py
@@ -3,26 +3,40 @@
 Created on Sat Jul 27 08:57:12 2019
 
 @author: Reuben
+
+The wire module is about simple 'Wire' classes. Wire instances can be connected
+to one, and only one, receiver (a callable). Unlike Signals, Wire instances
+hold normal a normal reference to the callable.
+
 """
 
 class WireContainer(dict):
-    ''' Holds a collection of signals '''
+    ''' A dictionary-like collection of Wire instances '''
     
     def wire(self, name=None, doc=None):
+        ''' Get a new Wire instance 
+        
+        Args:
+            name (str): A name of the wire [optional]
+            doc (str): A documentation string for the wire [optional]
+        '''
         name = len(self) if name is None else name
         w = Wire(name=name, doc=doc)
         self[name] = w
         return w
     
     def mute_all(self):
+        ''' Mute all wires in the container '''
         for key, wire in self.items():
             wire.mute()
 
     def unmute_all(self):
+        ''' Unmute all wires in the container '''
         for key, wire in self.items():
             wire.unmute()
 
     def reset_all(self):
+        ''' Reset all wires in the contain '''
         for key, wire in self.items():
             wire.reset()
     
@@ -32,15 +46,28 @@ wire = default_wire_container.wire
 
 
 class Wire():
+    ''' A simple instance that can be connected to one receiver 
+    
+        Args:
+            name (str): A name of the wire [optional]
+            doc (str): A documentation string for the wire [optional]    
+    '''
+    
     def __init__(self, name=None, doc=None):
         self._name = name
         self._doc = doc
         self.reset()
         
     def _emit(self):
+        ''' The default, unconnected, method '''
         raise AttributeError('Wire instance is not connected')
     
     def connect(self, receiver):
+        ''' Connect the wire to a callable receiver 
+        
+        Args:
+            receiver (callable): A receiver called by the wire.
+        '''
         if self.emit != self._emit:
             raise AttributeError('Wire instance is already connected and'
                                  + 'must first be disconnected.')
@@ -48,20 +75,25 @@ class Wire():
         self.fetch = receiver
     
     def disconnect(self):
+        ''' Disconnect the wire from its receiver '''
         self.reset()
         
     def _muted(self):
+        ''' The default muted method '''
         pass
         
     def mute(self):
+        ''' Prevent the wire from calling the receiver '''
         self._old = self.emit
         self.emit = self._muted
 
     def unmute(self):
+        ''' Allow the wire to call the receiver '''
         self.emit = self._old
         del self._old
         
     def reset(self):
+        ''' Fully reset the wire, disconnecting it if required '''
         self.emit = self._emit
         self.fetch = self._emit
         
